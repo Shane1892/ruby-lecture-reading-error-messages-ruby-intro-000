90a98c6ea841a5ce65c6a957559a9ab50b94ae57
Shane Adams <shane.adams2@icloud.com>
2020-03-30 04:01:02 +0000

    Automatically backed up by Learn

--- spec/no_ruby_errors_spec.rb
+++ spec/no_ruby_errors_spec.rb
@@ -1,34 +1,37 @@
 describe "Not having any errors and being all green" do
-
+end
   context 'NameError' do
     it 'raises a NameError when encountering undefined barewords' do
       expect{
         load './lib/a_name_error.rb'
       }.to_not raise_error
-    end
-  end
+end
+
+
 
   context 'SyntaxError' do
     it 'raises a SyntaxError for nonsensical code' do
       expect{
         load './lib/a_syntax_error.rb'
       }.to_not raise_error
-    end
-  end
+end
+
+
 
   context 'TypeError' do
+  end
     it 'raises a TypeError for objects of the wrong type' do
       expect{
         load './lib/a_type_error.rb'
       }.to_not raise_error
-    end
-  end
+end
+
 
   context 'ZeroDivisionError' do
     it 'raises a ZeroDivisionError for dividing by zero' do
       expect{
+      end
         load './lib/a_division_by_zero_error.rb'
       }.to_not raise_error
-    end
+    end-of-input
   end
-end
